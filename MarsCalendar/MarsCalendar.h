466bf2af1047ac6f5321376b86cedd584a18db9d
steve <srichey@gowithfloat.com>
2018-04-26 20:19:06 -0500

    Initial Commit

---
+++ MarsCalendar/MarsCalendar.h
@@ -0,0 +1,19 @@
+//
+//  MarsCalendar.h
+//  MarsCalendar
+//
+//  Created by ad laos on 4/26/18.
+//  Copyright © 2018 STVR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MarsCalendar.
+FOUNDATION_EXPORT double MarsCalendarVersionNumber;
+
+//! Project version string for MarsCalendar.
+FOUNDATION_EXPORT const unsigned char MarsCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarsCalendar/PublicHeader.h>
+
+
